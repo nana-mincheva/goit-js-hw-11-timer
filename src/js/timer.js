a41d71854228bcbfc34008e157839fac99719b1c
Nadya Mincheva <7000585m1@gmail.com>
2021-10-25 11:16:52 +0300

    add

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,31 +1,54 @@
-const refs = {
-  days: document.querySelector('[data-value = "days"]'),
-  hours: document.querySelector('[data-value = "hours"]'),
-  mins: document.querySelector('[data-value = "mins"]'),
-  secs: document.querySelector('[data-value = "secs"]'),
-};
-
 class CountdownTimer {
-  constructor({ targetDate }) {
-    this.targetDate = targetDate;
-    setInterval(this.timer, 1000);
+  constructor(selector, targetDate) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = new Date(targetDate);
   }
-  timer = () => {
-    function pad(value) {
-      return String(value).padStart(2, "0");
-    }
-    const currentTime = Date.now();
-    const deltaTime = this.targetDate - currentTime;
-    const days = pad(Math.floor(deltaTime / (1000 * 60 * 60 * 24)));
-    refs.days.textContent = days;
-    const hours = pad(
-      Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+
+  startCountdown() {
+    this.intervalId = setInterval(() => {
+      this.currentTime = new Date();
+      this.leftTime = this.targetDate - this.currentTime;
+      this.timeCounter(), this.includTimeIntoMarckup();
+      if (this.leftTime < 0) {
+        this.stopCountdown();
+      }
+    }, 1000);
+  }
+
+  timeCounter() {
+    this.days = Math.floor(this.leftTime / (1000 * 60 * 60 * 24));
+    this.hours = Math.floor(
+      (this.leftTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-    refs.hours.textContent = hours;
-    const mins = pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)));
-    refs.mins.textContent = mins;
-    const secs = pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
-    refs.secs.textContent = secs;
-  };
+    this.mins = Math.floor(
+      (this.leftTime % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    this.secs = Math.floor((this.leftTime % (1000 * 60)) / 1000);
+  }
+
+  includTimeIntoMarckup() {
+    this.refs = {
+      days: document.querySelector(" [data-value=days]"),
+      hours: document.querySelector(" [data-value=hours]"),
+      minutes: document.querySelector(" [data-value=mins]"),
+      seconds: document.querySelector(" [data-value=secs]")
+    };
+    this.refs.days.textContent = this.pad(this.days);
+    this.refs.hours.textContent = this.pad(this.hours);
+    this.refs.minutes.textContent = this.pad(this.mins);
+    this.refs.seconds.textContent = this.pad(this.secs);
+  }
+  stopCountdown() {
+    clearInterval(this.intervalId);
+    this.leftTime = 0;
+    this.timeCounter(this.leftTime);
+    this.includTimeIntoMarckup(this.leftTime);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
 }
-new CountdownTimer({ targetDate: new Date("Jan 19, 2022") });
+
+const countdownTimer = new CountdownTimer("#timer-1", "October 25, 2022, 11:15");
+countdownTimer.startCountdown();
